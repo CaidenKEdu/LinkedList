734bd03fdea665c5e015265fa0538bcb79d246c5
ckunitsky <ckunitsky@thegraysonschool.org>
2023-11-16 14:26:32 -0500

    Updated Project

--- main.py
+++ main.py
@@ -1,34 +1,70 @@
 class Node:
     def __init__(self, val=None):
-        if val != None:
-            self.val = val
+        if val is not None:
+            self.value = val
             self.next = Node()
         else:
-            self.val = None
+            self.value = None
             self.next = None
+
     def append(self, val):
-        if self.val != None:
+        if self.value is not None:
             self.next.append(val)
         else:
-            self.val = val
+            self.value = val
             self.next = Node()
+
     def __str__(self):
-        if self.val != None:
-            outString = str(self.val)
-            outString += ", "
-            outString += str(self.next)
-            return outString
+        return str(self.value)
+
+class LinkedList:
+    def __init__(self, values):
+        self.head = Node()
+        self.curr = self.head
+        self.len = 0
+        try:
+            self.concat(values)
+        except TypeError:
+            self.append(values)
+
+    def append(self, val):
+        self.len += 1
+        self.head.append(val)
+
+    def concat(self, values):
+        for item in values:
+            self.append(item)
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.curr.value is not None:
+            out = self.curr.value
+            self.curr = self.curr.next
+            return out
         else:
-            return ''
+            self.curr = self.head
+            raise StopIteration
+
+    def __str__(self):
+        out = '['
+        for item in self:
+            out += str(item) + ', '
+        return out[:-2] + ']'
+
+    def __len__(self):
+        return self.len
 
 
 def main():
-    myList = Node()
-    myList.append(1)
-    myList.append(2)
-    myList.append(3)
-    print(myList)
+    mylist1 = LinkedList([1, 2, 3])
+    mylist2 = LinkedList([4, 5, 6])
+    mylist = LinkedList(mylist1)
+    mylist.concat(mylist2)
+    print(mylist)
+    print(len(mylist))
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
