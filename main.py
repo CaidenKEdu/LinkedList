a559129083e5fa87dce4bb4b7744a1f96d138b01
ckunitsky <ckunitsky@thegraysonschool.org>
2023-12-05 14:23:22 -0500

    Updated Project

--- main.py
+++ main.py
@@ -56,14 +56,169 @@ class LinkedList:
     def __len__(self):
         return self.len
 
+    def __getitem__(self, item):
+        if item in range(-self.len, self.len):
+            curr = self.head
+            item %= self.len
+            for _ in range(item):
+                curr = curr.next
+            return curr.value
+        else:
+            raise IndexError(f'Index {item} is out of range for list of length {self.len}.')
+
+    def __setitem__(self, key, value):
+        if key in range(-self.len, self.len):
+            curr = self.head
+            key %= self.len
+            for _ in range(key):
+                curr = curr.next
+            curr.value = value
+        else:
+            raise IndexError(f'Index {key} is out of range for list of length {self.len}.')
+
+    def __delitem__(self, key):
+        if key in range(-self.len, self.len):
+            curr = self.head
+            key %= self.len
+            for i in range(self.len):
+                if i < key:
+                    curr = curr.next
+                elif i < self.len - 1:
+                    curr = curr.next
+                    self[i] = curr.value
+                else:
+                    self[i] = None
+                    self.len -= 1
+        else:
+            raise IndexError(f'Index {key} is out of range for list of length {self.len}.')
+    def insert(self, key, item):
+        if key in range(-self.len, self.len):
+            self.append(self[self.len - 1])
+            curr = self.head
+            key %= self.len
+            for i in range(self.len):
+                if i < key:
+                    curr = curr.next
+                elif i == key:
+                    temp2 = 0
+                    temp = self[i]
+                    self[i] = item
+                elif i <= self.len:
+                    temp2 = temp
+                    temp = self[i]
+                    self[i] = temp2
+        else:
+            raise IndexError(f'Index {key} is out of range for list of length {self.len}.')
+
+    def swap(self, a, b):
+        prev = self[a]
+        self[a] = self[b]
+        self[b] = prev
+
+    def getListType(self):
+        temp = 0
+        for i in self:
+            if type(i) == int:
+                if temp == 0:
+                    temp = 0
+                else:
+                    temp = 2
+                    break
+            elif type(i) == str:
+                if i == self[0]:
+                    temp = 1
+                elif temp == 1:
+                    temp = 1
+                else:
+                    temp = 2
+                    break
+            else:
+                temp = 2
+                break
+        if temp == 0:
+            return 'int'
+        elif temp == 1:
+            return 'str'
+        else:
+            return 'mix'
+
+    def isSorted(self):
+        if self.getListType() == 'int':
+            temp = 0
+            for i in range(self.len-1):
+                if self[i] <= self[i+1]:
+                    temp = 0
+                else:
+                    temp = 1
+                    break
+            if temp == 0:
+                return True
+            else:
+                return False
+        elif self.getListType() == 'str':
+            temp = 0
+            for i in range(self.len-1):
+                if len(self[i]) <= len(self[i + 1]):
+                    temp = 0
+                else:
+                    temp = 1
+                    break
+            if temp == 0:
+                return True
+            else:
+                return False
+        else:
+            raise TypeError("Objects In List Are Not Sortable")
+
+    def maxIdx(self):
+        if self.isSorted() == True:
+            return (self.len - 1)
+        else:
+            if self.getListType() == 'int':
+                largest = self[0]
+                for i in range(self.len-1):
+                    if largest < self[i + 1]:
+                        largest = self[i + 1]
+                return self.find(largest)
+            elif self.getListType() == 'str':
+                largest = self[0]
+                for i in range(self.len-1):
+                    if len(largest) < len(self[i + 1]):
+                        largest = self[i + 1]
+                return self.find(largest)
+            else:
+                raise TypeError("Objects In List Are Not Sortable")
+
+    def minIdx(self):
+        if self.isSorted() == True:
+            return 0
+        else:
+            if self.getListType() == 'int':
+                largest = self[0]
+                for i in range(self.len-1):
+                    if largest > self[i + 1]:
+                        largest = self[i + 1]
+                return self.find(largest)
+            elif self.getListType() == 'str':
+                largest = self[0]
+                for i in range(self.len-1):
+                    if len(largest) > len(self[i + 1]):
+                        largest = self[i + 1]
+                return self.find(largest)
+            else:
+                raise TypeError("Objects In List Are Not Sortable")
+
+    def find(self, value):
+        for i in range(self.len):
+            if self[i] == value:
+                return i
+        return None
 
 def main():
-    mylist1 = LinkedList([1, 2, 3])
-    mylist2 = LinkedList([4, 5, 6])
-    mylist = LinkedList(mylist1)
-    mylist.concat(mylist2)
-    print(mylist)
-    print(len(mylist))
+    mylist1 = LinkedList([1, 2, 3, 4, 5])
+    print(mylist1.minIdx())
+
+
 
 
 if __name__ == '__main__':
